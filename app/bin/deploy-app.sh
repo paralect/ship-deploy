8c87e92a070005c672da137429802a0100763d53
Nikita Nesterenko <nesterenko.nikita.sergeevich@gmail.com>
2020-07-09 14:13:45 +0300

    remove useless deploy scripts

--- app/bin/deploy-app.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd "$( dirname "${BASH_SOURCE[0]}" )"/../
-
-read -p "App to deploy: api | web | landing | admin: " appname
-
-helm upgrade --install "apps-$appname" ./ --namespace app \
-  --set appname=$appname \
-  --set imagesVersion="$(git rev-parse --verify HEAD)" \
-  -f ./values/values.yml

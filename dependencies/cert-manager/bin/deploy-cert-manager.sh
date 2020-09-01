29727ba9cfd4cb7875dfba6626dbb41d3f076e09
Nikita Solosnuk <n.solosnuk@paralect.com>
2020-09-01 15:23:25 +0300

    Configure cert manager deployment script

--- dependencies/cert-manager/bin/deploy-cert-manager.sh
+++ dependencies/cert-manager/bin/deploy-cert-manager.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"/../
 
-read -p "cluster issuer acme email: " email
-
-kubectl apply --validate=false \
-  -f https://github.com/jetstack/cert-manager/releases/download/v0.15.0/cert-manager.yaml \
-
 kubectl create namespace cert-manager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm install --name cert-manager --namespace cert-manager --version v0.15.0 jetstack/cert-manager
 
+helm upgrade --install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --version v0.16.1 \
+  --set installCRDs=true \
+  --set ingressShim.defaultIssuerName=letsencrypt \
+  --set ingressShim.defaultIssuerKind=ClusterIssuer
+
+read -p "cluster issuer acme email: " email
 helm upgrade --install cert-cluster-issuer ./cert-cluster-issuer --namespace cert-manager \
   -f ./cert-cluster-issuer/values/values.yml \
-  --set clusterIssuer.acme.email=$email \
+  --set clusterIssuer.acme.email=$email
